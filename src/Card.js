b29c1a2b97900d36370943bff38d48c66611e3da
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-10-30 03:16:21 +0530

    style: Design basic card design

--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import classes from './Card.module.css';
 
 class Card extends Component {
   isInvalid(input) {
     return input === null || input === undefined || input === '';
   }
-  
-  render () {
+
+  render() {
     const { card, name, expiry, cvv } = this.props;
     return (
-      <div>
-        <div>{ this.isInvalid(card) ? <div>#### #### #### ####</div> : card }</div>
-        <div>{ this.isInvalid(name) ? <div>FULL NAME</div> : name }</div>
-        <div>{ this.isInvalid(expiry) ? <div>MM YY</div> : expiry }</div>
-        <div>{ this.isInvalid(cvv) ? <div>***</div> : cvv }</div>
+      <div className={classes.card}>
+        <div className={classes.cardType}></div>
+        <div className={classes.number}>
+          {this.isInvalid(card) ? <div>#### #### #### ####</div> : card}
+        </div>
+        <div className={classes.horizontal}>
+          <div>
+            <div className={classes.title}>Cardholder Name</div>
+            <div className={classes.name}>{this.isInvalid(name) ? <div>FULL NAME</div> : name}</div>
+          </div>
+          <div>
+            <div className={classes.title}>Expiry</div>
+            <div className={classes.expiry}>
+              {this.isInvalid(expiry) ? <div>MM YY</div> : expiry}
+            </div>
+          </div>
+        </div>
+        <div>{this.isInvalid(cvv) ? <div>***</div> : cvv}</div>
       </div>
-    )
+    );
   }
 }
 
-export default Card
+export default Card;
